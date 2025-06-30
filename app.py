a4c3b33d235d5944949fcb0c001fbe7a93a9a557
cmhfahim <cmhfahim@gmail.com>
2025-06-30 11:50:38 +0600

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ if page == "🏠 Home":
     st.markdown("---")
 
     # Add vertical spacing after the intro block
-    st.markdown("<div style='height:40px'></div>", unsafe_allow_html=True)
+    st.markdown("<div style='height:100px'></div>", unsafe_allow_html=True)
 
     # Our Team heading
     st.markdown("### 👨‍💻 Our Team")
