725ed9912147157e6a9775077e9253568a330396
cmhfahim <cmhfahim@gmail.com>
2025-06-30 12:25:57 +0600

    Update app.py

--- app.py
+++ app.py
@@ -135,23 +135,13 @@ if page == "🏠 Home":
     # New Description Section after team members
     st.markdown("""
         <div style="text-align: center; max-width: 900px; margin: 0 auto; color:#241717; font-size: 18px; line-height: 1.6;">
+            <h2>Description</h2>
             <p>
-                Explore trends, visualize insights, and predict future movement of stocks from Dhaka Stock Exchange using interactive tools.
-            </p>
-            <p>
-                This platform leverages historical data to understand stock behavior and uses machine learning models (LightGBM) to forecast whether a company's stock is likely to go up, stay unchanged, or go down.
-            </p>
-            <p>
-                With rich visualizations, stock-wise filtering, and an interactive prediction interface, users can gain deeper insights into the market's rhythm.
-            </p>
-            <p>
-                Whether you're a curious learner, a data enthusiast, or a researcher, DeepMarket offers a compact yet powerful window into financial analytics.
-            </p>
-            <p>
-                Built using <strong>Python, Streamlit, Plotly, LightGBM, Pandas,</strong> and <strong>Seaborn</strong>, this project aims to bridge the gap between data science and financial decision-making.
+                Explore trends, visualize insights, and predict future movement of stocks from Dhaka Stock Exchange using interactive tools. This platform leverages historical data to understand stock behavior and uses machine learning models (LightGBM) to forecast whether a company's stock is likely to go up, stay unchanged, or go down. With rich visualizations, stock-wise filtering, and an interactive prediction interface, users can gain deeper insights into the market's rhythm. Whether you're a curious learner, a data enthusiast, or a researcher, DeepMarket offers a compact yet powerful window into financial analytics. Built using <strong>Python, Streamlit, Plotly, LightGBM, Pandas,</strong> and <strong>Seaborn</strong>, this project aims to bridge the gap between data science and financial decision-making.
             </p>
         </div>
     """, unsafe_allow_html=True)
+
     st.markdown(
         "<p style='text-align:center; margin-top:50px; color:black;'>💡 Built by <strong>Team QuantumTalk</strong></p>",
         unsafe_allow_html=True
